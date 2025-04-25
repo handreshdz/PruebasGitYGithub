2bf5ad7c897b335ddfffe3b7777833e5724b6d44
Andres Hernandez <105754182+handreshdz@users.noreply.github.com>
2025-04-24 22:41:27 -0500

    Update Principal.java
    Se adiciona nueva Línea de código

--- src/com/handres/pruebas/modelos/Principal.java
+++ src/com/handres/pruebas/modelos/Principal.java
@@ -3,5 +3,7 @@ package com.handres.pruebas.modelos;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+
+        System.out.println("Se adiciona nueva linea ");
     }
 }
